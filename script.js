39862e9a0449924941d8ce1daea19985367bcbd9
Joshua Paul <mars@joshuas-mbp.mynetworksettings.com>
2023-03-16 22:55:07 -0400

    Finished Homework

--- script.js
+++ script.js
@@ -7,41 +7,65 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   // Random quote of the day generator
-  const randomQuote = function() {
+  const randomQuote = function () {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
-  
+
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  let mainT = document.querySelector('#main-title')
 
   // Part 2
-
-
+  let body = document.querySelector('body')
+  body.style.backgroundColor = '#8170FF'
   // Part 3
-
+  let domFTL = document.querySelector('#favorite-things')
+  domFTL.childNodes[11].remove()
 
   // Part 4
-
+  let specialT = document.querySelectorAll('.special-title')
+  for (let ele of specialT) {
+    ele.style.fontSize = '2rem'
+  }
 
   // Part 5
-
+  let pastRaces = document.querySelector('#past-races')
+  pastRaces.childNodes[7].remove()
 
   // Part 6
-
+let newElement = document.createElement('li')
+ newElement.innerText = 'New York'
+ pastRaces.appendChild(newElement)
 
   // Part 7
-
+let newDiv = document.createElement('div')
+newDiv.classList.add('blog-post')
+newDiv.classList.add('purple')
+newDiv.innerHTML = '<h1> New york </h1> <p> I raced through new York </p>'
+let main = document.querySelector('.main')
+main.appendChild(newDiv)
 
   // Part 8
-
+  let quoteTitle = document.querySelector('#quote-title')
+  quoteTitle.addEventListener('click', randomQuote)
 
   // Part 9
+  let blogPosts = document.querySelectorAll('.blog-post')
+  for (let blogPost of blogPosts){
+    blogPost.addEventListener('mouseout', () => {
+      blogPost.classList.remove('red')
+      blogPost.classList.add('purple')
+     })
 
+     blogPost.addEventListener('mouseenter', () => {
+      blogPost.classList.remove('purple')
+      blogPost.classList.add('red')
+    })
+  }
 
 
 
